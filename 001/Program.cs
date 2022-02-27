4bf4157a40deda7cd37e22ab67062424ab7ee0bf
Vsevolod <sevakuzya@mail.ru>
2022-02-27 12:27:00 +0300

    1 tema na C#

---
+++ 001/Program.cs
@@ -0,0 +1,9 @@
+﻿
+Console.Write("Введите число ");
+Double a = Convert.ToDouble(Console.ReadLine());
+Double kvadrat = a*a;
+
+Console.Write("Квадрат ");
+Console.Write(a);
+Console.Write(" равен ");
+Console.WriteLine(kvadrat);
