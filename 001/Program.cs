4a521ec20c3afd66caa9364c381d5c05f34d7a7e
Vsevolod Kuzmin <99275779+PEBU3OP1@users.noreply.github.com>
2022-02-28 10:12:56 +0300

    Delete 001 directory

--- 001/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿
-Console.Write("Введите число ");
-Double a = Convert.ToDouble(Console.ReadLine());
-Double kvadrat = a*a;
-
-Console.Write("Квадрат ");
-Console.Write(a);
-Console.Write(" равен ");
-Console.WriteLine(kvadrat);
