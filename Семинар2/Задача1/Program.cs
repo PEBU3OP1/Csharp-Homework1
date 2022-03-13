92d5f5358edfeac02ed1102eb92138001d12fec9
Vsevolod <sevakuzya@mail.ru>
2022-03-13 16:22:31 +0300

    2 zadachi 2 seminar

---
+++ Семинар2/Задача1/Program.cs
@@ -0,0 +1,43 @@
+﻿ void SecondNum(int num){
+ 
+   
+
+if (num > 0 && num < 100){
+
+    Console.Write("Число не 3 значное!!!");
+   
+}
+
+else if (num > 100 && num < 1000){
+
+    int n1 = Math.Abs(num % 100);
+    int n2 = Math.Abs(n1 / 10);
+    Console.Write("Вторая цифра введенного числа: " + n2);
+   
+}
+
+else if (num > 1000 && num < 10000){
+
+    int n1 = Math.Abs(num % 1000);
+    int n2 = Math.Abs(n1 / 100);
+    Console.Write("Вторая цифра введенного числа: " + n2);
+   
+}
+
+else if (num > 10000 && num < 100000){
+
+    int n1 = Math.Abs(num % 10000);
+    int n2 = Math.Abs(n1 / 1000);
+    Console.Write("Вторая цифра введенного числа: " + n2);
+   
+}
+else{
+    Console.Write("Мне это надоело! Проще через массив");
+        
+}
+   
+}
+
+Console.WriteLine("Введите 3 значное число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+SecondNum(num);    
---
+++ Семинар2/Задача2/Program.cs
@@ -0,0 +1,21 @@
+﻿void ThirdDigitByArray (int num){
+
+
+    char[] arr = num.ToString().ToCharArray();
+    if (arr.Length < 3)
+    {
+    Console.WriteLine("третьей цифры нет");
+    }
+
+
+    else
+    {
+    Console.WriteLine("Третья цифра: " + arr[2]);
+    }
+}
+
+
+Console.WriteLine("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+ThirdDigitByArray(num);
