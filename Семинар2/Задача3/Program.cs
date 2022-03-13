40bb62acf93347ee6b6c1d86bdfe42b244b7e61b
Vsevolod <sevakuzya@mail.ru>
2022-03-13 16:44:28 +0300

    Homework 2

---
+++ Семинар2/Задача3/Program.cs
@@ -0,0 +1,24 @@
+﻿void Weekend (int day){
+int dayoff = day % 7;
+
+    if (dayoff == 6){
+    Console.WriteLine("Да, это суббота!");
+
+    }
+
+    else if (dayoff == 0){
+    Console.WriteLine("Да, это воскресенье!");
+
+    }
+
+    else {
+    Console.WriteLine("Нет!");
+
+    }
+
+}
+
+Console.WriteLine("Введите день недели: ");
+int day = Convert.ToInt32(Console.ReadLine());
+
+Weekend(day);
