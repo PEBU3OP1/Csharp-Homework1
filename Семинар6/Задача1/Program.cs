0295e7b0c491ec94035b54f226c67b51d58898e9
Vsevolod <sevakuzya@mail.ru>
2022-04-03 15:38:02 +0300

    Seminar6 ready

---
+++ Семинар6/Задача1/Program.cs
@@ -0,0 +1,34 @@
+﻿/*Задача 1: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+0, 7, 8, -2, -2 -> 2
+1, -7, 567, 89, 223-> 3
+*/
+
+
+
+void PozitivNumbers()
+{
+    int count = 0;
+    Console.Write("Enter quantity of numbers: ");
+    int M = Convert.ToInt32(Console.ReadLine());
+
+
+    for (int i = 1; i <= M; i++)
+    {
+        Console.Write($"Enter number {i}: ");
+        int num = Convert.ToInt32(Console.ReadLine());
+
+
+        if (num > 0) count++;
+
+    }
+    if (count == 0) Console.WriteLine("You didn't enter positive numbers!");
+    else
+    {
+        Console.WriteLine();
+        Console.WriteLine("Quantity of positive numbers: " + count);
+
+    }
+
+}
+PozitivNumbers();
---
+++ Семинар6/Задача2/Program.cs
@@ -0,0 +1,35 @@
+﻿/*Задача 2: Пользователь вводит с клавиатуры кол-во строк и столбцов для массива. Вывести массив в виде матрицы, создав "рамку" из единиц, а внутреннюю зону матрицы заполнить нулями.
+
+3, 4 ->
+1 1 1 1
+1 0 0 1
+1 1 1 1
+*/
+
+
+void DoubleArray()
+{
+    Console.Write("Enter q-ty of rows: ");
+    int rows = Convert.ToInt32(Console.ReadLine());
+
+    Console.Write("Enter q-ty of columns: ");
+    int columns = Convert.ToInt32(Console.ReadLine());
+
+
+    int [,] ar = new int [rows, columns];
+
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            if(i == 0 || i == rows-1) ar[i,j] = 1;
+            else if (j == 0 || j == columns-1) ar[i,j] = 1; 
+            else ar[i,j] = 0;
+            Console.Write(ar[i,j] + " ");
+        }
+        Console.WriteLine();
+    }
+
+}
+
+DoubleArray();
