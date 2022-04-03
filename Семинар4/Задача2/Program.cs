ab0d68dc2484ac3857d8230d73fee88039a0e280
Vsevolod <sevakuzya@mail.ru>
2022-04-03 15:12:45 +0300

    Seminar4 ready

---
+++ Семинар4/Задача2/Program.cs
@@ -0,0 +1,64 @@
+﻿/*Задача 2: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+452 -> 11
+82 -> 10
+9012 -> 12
+*/
+
+
+Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int sum = 0;
+
+
+while (true)
+{
+    int a = num % 10;
+    sum = sum + a;
+    num = num / 10;
+
+    if (num / 10 == 0)
+    {
+        sum = sum + num;
+        break;
+    }
+}
+
+
+Console.WriteLine("Сумма с помощью деления и остатков: " + Math.Abs(sum));
+
+
+
+
+
+void Main()
+{
+    Console.Write("Введите число: ");
+    string num = Console.ReadLine();
+
+
+
+    char[] ar = num.ToCharArray();
+    int sum = 0;
+
+    if (num[0] == 45)
+    {
+        for (int i = 1; i < ar.Length; i++)
+        {
+            int b = Convert.ToInt32(Convert.ToString(ar[i]));
+            sum = sum + b;
+        }
+        Console.WriteLine("Сумма через массив: " + sum);
+    }
+    else
+    {
+        for (int i = 0; i < ar.Length; i++)
+        {
+            int b = Convert.ToInt32(Convert.ToString(ar[i]));
+            sum = sum + b;
+        }
+        Console.WriteLine("Сумма через массив: " + sum);
+    }
+}
+
+Main();
---
+++ Семинар4/Задача3/Program.cs
@@ -0,0 +1,22 @@
+﻿
+/*Задача 3: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+6, 1, 33 -> [6, 1, 33]
+*/
+
+void NewMasive ()
+{
+    string [] ar = new string [8];
+    for (int i = 0; i < ar.Length; i++)
+    {
+        Console.WriteLine($"Введите {i+1} член массива: ");
+        ar[i] = Console.ReadLine();
+
+    }
+    Console.WriteLine();
+    Console.WriteLine("Вот Ваш массив: ");
+    foreach(string s in ar) Console.Write(s +" ");
+}
+
+NewMasive();
