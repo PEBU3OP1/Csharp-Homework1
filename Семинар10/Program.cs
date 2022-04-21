13f0bffe9f74e873b7a9f0855fb40e8992083bcb
Vsevolod <sevakuzya@mail.ru>
2022-04-21 23:44:33 +0300

    Homework10 is done

---
+++ Семинар10/Program.cs
@@ -0,0 +1,25 @@
+﻿int[] data = { 0, 1, 1, 1, 1, 0, 0, 0, 1, 1, 0, 1, 0 }; // добавил еще 10 для проверки
+int[] info = { 2, 3, 3, 1, 4 };
+
+int[] ar = new int[info.Length];
+int SchetNizh = 0;
+int SChetVerh = 0;
+
+Console.WriteLine("Вот Ваш результат: ");
+
+for (int i = 0; i < info.Length; i++)
+{
+    double rezultat = 0;
+    double stepen = info[i] - 1;
+    SChetVerh = SChetVerh + info[i];
+
+    for (int j = SchetNizh; j < SChetVerh; j++)
+    {
+        rezultat = rezultat + (data[j] * Math.Pow(2, stepen));
+        stepen--;
+    }
+    SchetNizh = SchetNizh + info[i];
+
+    ar[i] = Convert.ToInt32(rezultat);
+    Console.Write(ar[i] + " ");
+}
